19047b697f14d66f17d38488d77f084e7f461075
chiakimz <chiakimizuta61@gmail.com>
2021-05-05 14:51:24 +0100

    finished writing rules

--- src/clara_practice/mini_practice.clj
+++ src/clara_practice/mini_practice.clj
@@ -4,7 +4,7 @@
 
 (defrecord BusinessOwner [name is-dormant through-broker])
 
-(defn make-businessowner [person]
+(defn make-business-owner [person]
   (let [default-values {:is-dormant false :through-broker false}]
     (map->BusinessOwner (merge default-values person))))
 
@@ -12,9 +12,47 @@
 
 (defrecord Documents [name doc])
 
-(defrecord SubmittedDocuments [name amount])
+(defrecord SubmittedAllDocuments [name amount])
 
 (defrecord Eligible [name])
 
+(defquery not-eligible-query?
+  []
+  [?not-eligible <- NotEligible])
+
+(defquery eligible?
+  []
+  [?eligible <- Eligible])
+
+(defquery submitted-documents?
+  []
+  [?x <- SubmittedAllDocuments])
+
+(defrule dormant-companies-are-not-eligible
+  [BusinessOwner (= ?name name) (= is-dormant true)]
+  =>
+  (insert! (->NotEligible ?name)))
+
+(defrule initial-details-provided
+  "Here it's assumed that if someone comes through a broker,
+  their initial details are automatically provided"
+  [BusinessOwner (= ?name name) (= through-broker true)]
+  =>
+  (insert-all! [(->Documents ?name :turnover)
+                (->Documents ?name :bank-statement)
+                (->Documents ?name :loan-amount-requested)]))
+
+(defrule need-to-submit-four-documents
+  [BusinessOwner (= ?name name)]
+  [?c <- (acc/count) from (Documents (= ?name name))]
+  [:test (> ?c 3)]
+  =>
+  (insert! (->SubmittedAllDocuments ?name ?c)))
 
 
+(defrule completed-documents-grants-eligibility
+  "Becomes eligible if you have submitted all documents"
+  [SubmittedAllDocuments (= ?name name)]
+  [:not [NotEligible (= ?name name)]]
+  =>
+  (insert! (->Eligible ?name)))
